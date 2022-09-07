56f4e1fb38f77be3d38318a97c43a24796877e7d
Daria-Russia <111522488+Daria-Russia@users.noreply.github.com>
2022-09-07 19:00:48 +0300

    Initial save

---
+++ Homework_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Task 10
+//int n = new Random(). Next(100, 1000);
+//Console.WriteLine(n);
+//Console.WriteLine((n-(n/100)*100-(n%10))/10);
+
+
+//Task 15
+//int n = Convert.ToInt32(Console.ReadLine());
+//if (n==6 || n==7)
+//Console.WriteLine("yes");
+//else
+//Console.WriteLine("no");
+
+//Ice cream
+int k = Convert.ToInt32(Console.ReadLine());
+if (k%3==0 || k%5==0)
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
